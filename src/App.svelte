d0c7bf8532352085e2055a7bcb17e31d273c9d7b
Daniel <opodaniel@yahoo.com>
2020-09-30 08:26:15 +0300

    - initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import TicTac from './TicTac.svelte';
+</script>
+
+<main>
+	Do your magic;
+	<TicTac />
+</main>
+
+<style>
+</style>
---
+++ src/TicTac.svelte
@@ -0,0 +1,41 @@
+<script>
+    let lastValue;
+    let elements;
+    const asignValue = (index) => {
+        const newValue = !lastValue || lastValue === 'o' ? 'x' : 'o';
+        ([elements[index], lastValue] = [newValue, newValue]);
+    }
+    function reset() {
+        debugger;
+        elements = new Array(9).fill('');
+    }
+    reset();
+</script>
+<h3>Tic Tac magic</h3>
+<div class="content">
+    {#each elements as element, i}
+        <div class="cell-{i + 1}" on:click={() => asignValue(i)}>{element}</div>
+    {/each}
+    <button type='button' on:click={reset}>Reset</button>
+</div>
+
+<style>
+    [class^='cell-'] {
+        width: 50px;
+        height: 50px;
+        border: 1px solid black;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 2em;
+        text-transform: uppercase;
+        cursor: pointer;
+    }
+
+    .content {
+        display: flex;
+        flex-wrap: wrap;
+        width: 190px;
+        margin: 0 auto;
+    }
+</style>
